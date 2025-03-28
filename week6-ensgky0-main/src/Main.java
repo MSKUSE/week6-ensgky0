2fccec9ccee9a3981d766ba6a128d5f656eb81a4
ensgky0 <enesgokyar@posta.mu.edu.tr>
2025-03-28 12:30:12 +0300

    Add files via upload

---
+++ week6-ensgky0-main/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Point p = new Point(3,4);
+        Rectangle r1 = new Rectangle(p,3,4);
+        System.out.println(r1.area());
+        System.out.println(r1.perimeter());
+
+    }
+}
